5ebbe3a3ff6cc997d3d44bc35e30d49b7daf382b
dmitriyvls <hlooba@mail.ru>
2023-03-03 13:29:43 +0300

    правки в 1 и 4 функции, переименование 3 функции

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,12 @@
 //функция 1
-const text = 'Hello world';
-function stringLength () {
-  const sybmbols = 10;
-  if(text.length >= sybmbols) {
+function stringLength (text, sybmbols) {
+  if(text.length <= sybmbols) {
     return true;
   } else {
     return false;
   }
 }
-stringLength('');
+stringLength('проверяемая строка', 20);
 
 //функция 2
 function checkPalindrome (str) {
@@ -18,22 +16,23 @@ function checkPalindrome (str) {
 checkPalindrome('Анна');
 
 //функция 3
-function checkNumber (str) {
+function getNumberFromString (str) {
   let numberResult = '';
   for (let i = 0; i < str.length; i++) {
-    if (!Number.isNaN(parseInt(str.at(i), 10))) {
+    if (!getNumberFromString.isNaN(parseInt(str.at(i), 10))) {
       numberResult += str.at(i);
     }
   }
   return parseInt(numberResult, 10);
 }
-checkNumber(2023);
+getNumberFromString(2023);
 //функция 4
 function checkString (str, minLength, pad) {
   const actual = minLength - str.length;
   if (actual <= 0) {
     return str;
   }
-  return pad.slice(0, pad % pad.length) + pad.repeat(actual / pad.length) + str;
+  return pad.slice(0, actual % pad.length) + pad.repeat(actual / pad.length) + str;
 }
 checkString('qwerty', 4, '0');
+
