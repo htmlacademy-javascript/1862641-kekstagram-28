26b9716c2dcfd11fb97371d2497071299b621f60
dmitriyvls <hlooba@mail.ru>
2023-03-26 00:38:16 +0300

    реализация полноразмерных фотографий

--- js/modal.js
+++ js/modal.js
@@ -1,45 +1,73 @@
 import { isEscapeKey } from './util.js';
+
 const bigPicture = document.querySelector('.big-picture');
-const pictureLinks = document.querySelectorAll('.picture');
 const bigPictureClose = bigPicture.querySelector('.big-picture__cancel');
 
-const onDocumentKeydown = (evt) => {
-  if(isEscapeKey(evt)) {
-    evt.preventDefault();
-    bigPicture.classList.add('hidden');
-  }
+const createCommemtItem = (comment) => {
+  const newComment = document.createElement('li');
+  const commentAvatar = document.createElement('img');
+  const commentText = document.createElement('p');
+  commentAvatar.classList.add('social__picture');
+  commentAvatar.src = comment.avatar;
+  commentAvatar.alt = comment.name;
+  commentAvatar.width = 35;
+  commentAvatar.height = 35;
+  commentText.textContent = comment.message;
+  commentText.classList.add('social__text');
+  newComment.classList.add('social__comment');
+  newComment.appendChild(commentAvatar);
+  newComment.appendChild(commentText);
+
+  return newComment;
 };
+
+const createBigPicture = (photos) => {
+  const bigPictureImg = bigPicture.querySelector('.big-picture__img img');
+  const bigPictureLikes = bigPicture.querySelector('.likes-count');
+  const bigPictureComments = bigPicture.querySelector('.comments-count');
+  const commentsList = bigPicture.querySelector('.social__comments');
+
+  for (let i = 0; i < photos.commemts.length; i++) {
+    const itemComment = createCommemtItem(photos.commemts[i]);
+    commentsList.appendChild(itemComment);
+  }
+  bigPictureImg.src = photos.url;
+  bigPictureLikes.textContent = photos.likes;
+  bigPictureComments.textContent = photos.commemts.length;
+};
+
 // показать модальное окно
 
-const showModal = () => {
-  for (const pictureLink of pictureLinks) {
-    pictureLink.addEventListener('click', (evt) => {
-      evt.preventDefault();
-      bigPicture.classList.remove('hidden');
+const showModal = (item, photo) => {
 
-      document.addEventListener('keydown', onDocumentKeydown);
-    });
-  }
-};
-showModal();
-
-//закрывае модальное окно
-const closeModal = () => {
-  bigPictureClose.addEventListener('click', () => {
-    bigPicture.classList.add('hidden');
+  item.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    createBigPicture(photo);
+    bigPicture.classList.remove('hidden');
+    document.addEventListener('keydown', onDocumentKeydown);
   });
 
-  document.addEventListener('keydown', onDocumentKeydown);
 };
 
-closeModal();
+//закрываеm модальное окно
+
+const close = () => {
+  bigPicture.classList.add('hidden');
+  document.removeEventListener('keydown', onDocumentKeydown);
+
+};
+
+const closeModal = () => {
+  bigPictureClose.addEventListener('click', close);
+};
 
 //закрываем модальное окно с клавиатуры
 
+function onDocumentKeydown (evt) {
+  if(isEscapeKey(evt)) {
+    bigPicture.classList.add('hidden');
+    close();
+  }
+}
 
-const closeModalEsc = () => {
-  bigPicture.classList.add('hidden');
-
-  document.removeEventListener('keydown', onDocumentKeydown);
-};
-closeModalEsc();
+export {showModal, closeModal};
