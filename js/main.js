3ce28a10374d40a9fc46fbb82636f45440ae8ecb
DmitriyVls <112543922+DmitriyVls@users.noreply.github.com>
2023-03-29 08:32:54 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 import { create } from './data.js';
 import {createPhotos, appendsPhoto} from './thumbnails.js';
-import {init} from './modal.js';
 
 const photosData = create();
 const photosView = createPhotos(photosData);
 appendsPhoto(photosView);
-init();
