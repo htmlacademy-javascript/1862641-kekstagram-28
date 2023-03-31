8f3c70b08b0e98211311fd4ebb0ecdb31f9133b1
dmitriyvls <hlooba@mail.ru>
2023-03-31 21:24:41 +0300

    валидация формы

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { create } from './data.js';
 import {createPhotos, appendsPhoto} from './thumbnails.js';
+import {} from './form.js';
 
 const photosData = create();
 const photosView = createPhotos(photosData);
